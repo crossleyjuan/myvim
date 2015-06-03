5aa126d30690474c900bb52bb7588f5c1f9ae879
Cross <crossleyjuan@gmail.com>
2015-06-03 07:45:22 +0000

    Install added

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp -R autoload ~/.vim/
+cp -R bundle ~/.vim/
+cp -R colors ~/.vim/
+cp -R c-support ~/.vim/
+cp -R doc ~/.vim/
+cp -R ftplugin ~/.vim/
+cp -R plugin ~/.vim/
+cp vimrc ~/.vimrc
